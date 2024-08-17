d320e434a06878d5a364de3771870aa99abadd0b
mbaker <mekenna.drabner@gmail.com>
2024-08-16 21:18:08 -0600

    Created database/tables in schema.sql/. Inserted values into tables  in seeds.sql. Created db, user, and pass in env

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO department (name) VALUES ('Human Resources');
+INSERT INTO department (name) VALUES ('Administration');
+INSERT INTO department (name) VALUES ('Accounting');
+INSERT INTO department (name) VALUES ('Sales');
+
+INSERT INTO role (title, salary, department_id)  VALUES ('Regional Manager', 60000, 1);
+INSERT INTO role (title, salary, department_id)  VALUES ('Human Resources', 65000, 2);
+INSERT INTO role (title, salary, department_id)  VALUES ('Accountant', 75000, 3);
+INSERT INTO role (title, salary, department_id)  VALUES ('Sales Represenative', 85000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Michael', 'Scott', 1, NULL);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Toby', 'Flenderson', 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Kevin', 'Malone', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Kevin', 'Malone', 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Jim', 'Halpert', 4, 1);
+
+
+
+
