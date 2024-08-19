50694090a0632442906eb7d97d6b42a033d222a3
mbaker <mekenna.drabner@gmail.com>
2024-08-19 13:34:56 -0600

    created main menu prompts in index

--- src/index.js
+++ src/index.js
@@ -4,3 +4,26 @@ import { getDepartments, getRoles,  getEmployees } from './queries';
 //import inquirer for prompts
 import inquirer from 'inquirer';
 
+const mainMenu = async () => {
+    const answers = await inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            options: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit'
+            ],
+        },
+    ]);
+
+
+    
+}
+
