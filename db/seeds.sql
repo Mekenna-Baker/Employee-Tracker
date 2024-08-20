20f7593bd6d1805ece6663487346b1e9567c1b83
mbaker <mekenna.drabner@gmail.com>
2024-08-19 19:22:03 -0600

    changed seed values back to original values to reflect postgres as I can't rerun schema and seeds yet

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,22 +1,22 @@
-INSERT INTO department (name) VALUES ('Management');
-INSERT INTO department (name) VALUES ('Kitchen');
-INSERT INTO department (name) VALUES ('Host');
-INSERT INTO department (name) VALUES ('Janitorial');
+INSERT INTO department (name) VALUES ('Administration');
+INSERT INTO department (name) VALUES ('Human Resources');
+INSERT INTO department (name) VALUES ('Accounting');
+INSERT INTO department (name) VALUES ('Sales');
 
 
 
-INSERT INTO role (title, salary, department_id)  VALUES ('Restaurant Owner', 40000, 1);
-INSERT INTO role (title, salary, department_id)  VALUES ('Line Cook', 35500, 2);
-INSERT INTO role (title, salary, department_id)  VALUES ('Hostess', 35000, 3);
-INSERT INTO role (title, salary, department_id)  VALUES ('Janitor', 30000, 4);
+INSERT INTO role (title, salary, department_id)  VALUES ('Regional Manager', 70000, 1);
+INSERT INTO role (title, salary, department_id)  VALUES ('Human Resources', 65000, 2);
+INSERT INTO role (title, salary, department_id)  VALUES ('Accountant', 65000, 3);
+INSERT INTO role (title, salary, department_id)  VALUES ('Sales Represenative', 60000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Bob', 'Belcher', 1, NULL);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Linda', 'Belcher', 2, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Michael', 'Scott', 1, NULL);
 
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Tina', 'Belcher', 3, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Gene', 'Belcher', 4, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Louise', 'Belcher', 5, 1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Toby', 'Flenderson', 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Kevin', 'Malone', 4, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Jim', 'Halpert', 5, 1);
 
 
 
